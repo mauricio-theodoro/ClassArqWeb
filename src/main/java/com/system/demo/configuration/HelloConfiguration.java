9780cfff9028a80020f0bd07df9bc6ad7970c606
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2025-02-28 19:55:39 -0300

    Atividade Prática 01 Arq. de Microsserviços

---
+++ src/main/java/com/system/demo/configuration/HelloConfiguration.java
@@ -0,0 +1,4 @@
+package com.system.demo.configuration;
+
+public class HelloConfiguration {
+}
---
+++ src/main/java/com/system/demo/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.system.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String helloWorld(String name) {
+        return "Hello World : " + name;
+    }
+}
